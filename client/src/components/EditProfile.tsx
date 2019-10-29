1bf87eca0fc323191bf994a2291624beef394229
Verdang <verdang@media-saturn.com>
2019-10-29 16:44:58 +0100

    change editProfil view mode

--- client/src/components/EditProfile.tsx
+++ client/src/components/EditProfile.tsx
@@ -1,4 +1,5 @@
 import React, { Component, ChangeEvent } from 'react'
+
 import mongoose from 'mongoose';
 import { IAction, ActionType } from '../framework/IAction';
 import { ILessonData, IState, IUser } from '../state/appState'
@@ -6,7 +7,6 @@ import axios from 'axios';
 import { reducerFunctions } from '../reducer/appReducer';
 import history from '../framework/history';
 import { IWindow } from '../framework/IWindow'
-import { Button } from 'react-bootstrap';
 
 declare let window: IWindow;
 interface IProps {
@@ -16,16 +16,6 @@ interface IProps {
 interface IJSXState {
   edit_mode: boolean;
   user: IUser;
-}
-
-interface IUserAction extends IAction {
-  user: IUser
-}
-
-reducerFunctions[ActionType.user_updated] = function (newState: IState, updateAction: IUserAction) {
-  console.log(updateAction.user);
-  newState.BM.user = updateAction.user;
-  return newState
 }
 
 export default class EditProfile extends React.PureComponent<IProps, IJSXState> {
@@ -44,70 +34,65 @@ export default class EditProfile extends React.PureComponent<IProps, IJSXState> 
   renderEditMode() {
     return (
       <div>
-        <form>
-          <div className="row" style={{ marginTop: '50px' }} >
-            <div className="col-md-2"></div>
-            <div className="col-md-3">
-              <div className="col">
-                <label htmlFor="firstName">First Name:</label>
-                <input className="form-control" name="handleFirstName" onChange={this.handleChange} value={this.state.user.firstname}></input>
-              </div>
+        <div className="RealEdit"><div>First Name: </div>
+          <input name="handleFirstName" onChange={this.handleChange} value={this.state.user.firstname}></input>
 
-              <div className="col">
-                <label htmlFor="lastName">Last Name:</label>
-                <input className="form-control" name="handleLastName" onChange={this.handleChange} value={this.state.user.lastname}></input>
-              </div>
+        </div>
+        <div className="RealEdit"><div>Last Name: </div>
+          <input name="handleLastName" onChange={this.handleChange} value={this.state.user.lastname}></input>
 
-              <div className="col">
-                <label htmlFor="username">Username:</label>
-                <input className="form-control" name="handleUserName" onChange={this.handleChange} value={this.state.user.username}></input>
-              </div>
+        </div>
+        <div className="RealEdit"><div>Username: </div>
+          <input name="handleUserName" onChange={this.handleChange} value={this.state.user.username}></input>
 
-              <div className="col">
-                <label htmlFor="password">Password:</label>
-                <input className="form-control" name="handlePassword" onChange={this.handleChange} value="****"></input>
-              </div>
-              
-          <Button type="submit" onClick={this.saveProfile} >Save</Button>
-            </div>
+        </div>
+        <div className="RealEdit"><div>Password</div>
+          <input name="handlePassword" onChange={this.handleChange} value={this.state.user.password}></input>
 
-          </div>
-        </form>
- 
-
-
-          
-    
+        </div>
       </div>
     )
   }
   renderViewMode() {
     return (
       <div>
-        <div className="Edit">
-          <p>Username: {window.CS.getBMState().user.username}</p>
+ <div className="row" style={{ marginTop: '50px' }} >
+  <div className="col-md-2"></div>
+  <div className="col-md-2">
 
-        </div>
+      <div className="Edit">
+          <p>Username: {window.CS.getBMState().user.username}</p>
+      </div>
+
         <div className="Edit">
           <p>First Name: {window.CS.getBMState().user.firstname}</p>
-
         </div>
+
         <div className="Edit">
           <p>Last Name: {window.CS.getBMState().user.lastname}</p>
-
         </div>
+
         <div className="Edit">
           <p>Password: ************</p>
-
         </div>
+
         <div className="Edit">
           <p>Picture: </p>
-
         </div>
 
+     </div>
+    </div>
+   <div className="row" style={{ marginTop: '5px' }}>
+   <div className="col-md-2"></div>
+ 
+
+  </div>
+  <div className="col-md-2"></div>
+        <div className="col-md-2">
         <button className="btn btn-primary" onClick={this.handleSwitchToEditMode}>Edit</button>
         <button className="btn btn-primary" onClick={this.handleLogout}>Logout</button>
-      </div>
+</div>
+  </div>
     )
   }
   render() {
@@ -152,53 +137,15 @@ export default class EditProfile extends React.PureComponent<IProps, IJSXState> 
     const target = event.target as HTMLInputElement
     const name = target.name as string;
     let newUser = this.state.user;
+    //newUser[name] = target.value;
     this.setState({
       user: newUser
     });
   }
 
-
-  saveProfile = (event: any) => {
-    event.preventDefault();
-    const aiAction: IAction = {
-      type: ActionType.server_called
-    }
-  window.CS.clientAction(aiAction);
-        const uiAction: IAction = {
-            type: ActionType.server_called
-        }
-        window.CS.clientAction(uiAction);
-        axios.post('/auth/signup', window.CS.getBMState().user)
-            .then(res => {
-                const uiAction: IAction = {
-                    type: ActionType.user_updated
-                }
-                history.push('/');
-                window.CS.clientAction(uiAction);
-
-                console.log(res.data)
-            });
-
-}}
+}
 
 //
-/*
-handleSubmit(event: any) {
-        event.preventDefault();
-        const uiAction: IAction = {
-            type: ActionType.server_called
-        }
-        window.CS.clientAction(uiAction);
-        axios.post('/auth/signup', window.CS.getBMState().user)
-            .then(res => {
-                const uiAction: IAction = {
-                    type: ActionType.user_created
-                }
-                history.push('/');
-                window.CS.clientAction(uiAction);
 
-                console.log(res.data)
-            });
-    }*/
 
 
