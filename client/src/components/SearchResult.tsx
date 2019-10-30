b0bc6b167628b1383477d8d58868a2a03fe31770
Ahrens <ahrensv@media-saturn.com>
2019-10-30 15:16:53 +0100

    confirmation after jacob changed

--- client/src/components/SearchResult.tsx
+++ client/src/components/SearchResult.tsx
@@ -25,51 +25,52 @@ class SearchResult extends Component<{}, IState> {
 
   render() {
     if (window.CS.getBMState().searchResult.length == 0) {
-    return (
-      <div className="row d-flex justify-content-start" style={{ marginTop: '50px' }} >
-  <div className="col-md-2"></div>
-  <div className="col-md-6 ">
-      <div className="jumbotron jumbotron-fluid">
-      <div className="container">
-        <h1 className="display-4">Sorry!</h1>
-        <p className="lead">We have currently no Teacher for your search...</p>
-      </div></div>
-    </div></div>
-    )
-  } else {
-    return (
-      <div>
-
-
-        {window.CS.getBMState().searchResult ? window.CS.getBMState().searchResult.map((item: any) =>
-          <>
-            <div className="row" style={{ marginTop: '2%' }} ></div>
+      return (
+        <div className="row d-flex justify-content-start" style={{ marginTop: '50px' }} >
+          <div className="col-md-2"></div>
+          <div className="col-md-6 ">
+            <div className="jumbotron">
+              <div className="container">
+                <h1 className="display-4">Sorry!</h1>
+                <p className="lead">We have currently no Teacher for your search...</p>
+              </div></div>
+          </div></div>
+      )
+    } else {
+      return (
+        <div>
             <h5 className="d-flex justify-content-center" >We found following results for your search:</h5>
 
+          {window.CS.getBMState().searchResult ? window.CS.getBMState().searchResult.map((item: any) =>
+            <>
+              <div className="row" style={{ marginTop: '2%' }} ></div>
+              
 
-            <div className="row " style={{ marginTop: '2%', marginBottom: '2%' }} key={item._id} >
 
-              <div className="col-md-2" ></div>
-              <div className="col-md-8 bg-primary" >
-                <img src={item.lesson_pictureURL} />
+              <div className="row " style={{ marginTop: '2%', marginBottom: '2%' }} key={item._id} >
 
-              <div className="d-flex justify-content-around bg-light"> <span>{item.lesson_name}</span>   <span>{item.lesson_location }</span>  <span>{item.lesson_price}€</span> </div>
-                
-              </div>
+                <div className="col-md-2" ></div>
+                <div className="col-md-8 bg-primary" >
+                  <img src={item.lesson_pictureURL} />
 
-              <div className="col-md-2 d-flex justify-content-center" style={{ marginTop: '2%' }}> <button className="btn btn-primary " id={item._id}><Link style={{ textDecoration: 'none', color: 'white' }} to={'/LessonDetail/read/' + item._id}>Show</Link></button></div>
-            </div></>) : null}
-        
-        <div className="row">
-          <div className=" col-md-4 col-xs-4" > </div>
-          <div className="col-md-4 d-flex justify-content-center" style={{ marginTop: '2%' }}>
-            <button className="btn btn-primary" onClick={this.handleQuery} >Back</button>
+                  <div className="d-flex justify-content-around bg-light"> <span>{item.lesson_name}</span>   <span>{item.lesson_location}</span>  <span>{item.lesson_price}€</span> </div>
+
+                </div>
+
+                <div className="col-md-2 d-flex justify-content-center" style={{ marginTop: '2%' }}> <button className="btn btn-primary " id={item._id}><Link style={{ textDecoration: 'none', color: 'white' }} to={'/LessonDetail/read/' + item._id}>Show</Link></button></div>
+              </div></>) : null}
+
+          <div className="row">
+            <div className=" col-md-4 col-xs-4" > </div>
+            <div className="col-md-4 d-flex justify-content-center" style={{ marginTop: '2%' }}>
+              <button className="btn btn-primary" onClick={this.handleQuery} >Back</button>
+            </div>
           </div>
         </div>
-      </div>
 
-    );
+      );
 
+    }
   }
-}}
+}
 export default SearchResult;
